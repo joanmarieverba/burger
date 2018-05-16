e9706ee20e5defda028ca2a2957609792c7854da
Joan Marie Verba <verba001@umn.edu>
2018-05-16 15:57:58 -0500

    miracle

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_ontroller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
