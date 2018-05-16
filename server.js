e8a06de67512b4dd03edbf152f4c9a352ad72be4
Joan Marie Verba <verba001@umn.edu>
2018-05-16 15:14:09 -0500

    more

--- server.js
+++ server.js
@@ -1,44 +1,29 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var app = express();
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Sets up the Express app to handle data parsing
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
 app.use(bodyParser.json());
 
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_ontroller.js");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "loremipsum",
-    database: "burgers_db"
-});
-
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-///lots of code goes here
-
-
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
