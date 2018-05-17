4952eebc5b412d7c8a3836054f0f619faba28feb
Joan Marie Verba <verba001@umn.edu>
2018-05-17 14:46:34 -0500

    more

--- config/orm.js
+++ config/orm.js
@@ -1,48 +1,11 @@
 // Import MySQL connection.
 let connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
-    let arr = [];
-
-    for (let i = 0; i < num; i++) {
-        arr.push("?");
-    }
-
-    return arr.toString();
-}
-
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-    let arr = [];
-
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-        let value = ob[key];
-        // check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
-            arr.push(key + "=" + value);
-        }
-    }
-
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
-}
-
 // Object for all our SQL statement functions.
 let orm = {
     selectAll: function (tableInput, cb) {
         let queryString = "SELECT * FROM " + tableInput + ";";
+        //let queryString = "SELECT * FROM " + tableInput + " WHERE devoured=false;";
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
@@ -56,9 +19,7 @@ let orm = {
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+        queryString += "VALUES (?, ?)";
 
         console.log(queryString);
 
@@ -70,6 +31,8 @@ let orm = {
             cb(result);
         });
     },
+
+
     // Update is to update devour from false to true
     updateOne: function (table, changeArray, cb) {
         var queryString = "UPDATE " + table + " SET ? WHERE ?";
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,8 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne(burgers, ["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+    console.log("req.body.burger_name ", req.body.burger_name);
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
             // Send back the ID of the new burger
             res.json({ id: result.insertId });
         });
