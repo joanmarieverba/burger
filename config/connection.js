a9d956f2bc841b1cd24aaecbb15e2892e72d3859
Joan Marie Verba <verba001@umn.edu>
2018-05-19 13:35:14 -0500

    code added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "loremipsum",
-    database: "burgers_db"
-});
+var connection;
+
+//connect to Heroku
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "loremipsum",
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
 connection.connect(function (err) {
